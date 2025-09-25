ed4279dd83a20b4fe80ec03ee6e5800b433d94c1
Juan_Diego <juan3312orrego@gmail.com>
2025-09-25 11:03:05 -0500

    CRUD Básico para categorias

--- academy-blue/public/js/custom.js
+++ academy-blue/public/js/custom.js
@@ -1,5 +1,5 @@
 function remove() {
-    var x = confirm("¿Está seguro de que desea eliminar el registro?");
+    let x = confirm("¿Está seguro de que desea eliminar el registro?");
     if (x)
         return true;
     else
